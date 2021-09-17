fe1454a7cbf90983de36757688bacae548c38e57
Tomas Ghilino <tomasghilino01@gmail.com>
2021-09-17 20:40:04 -0300

    base del header hecha + agrego imagenes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import './Header.css';
 
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <Fragment>
+      <div className="header">
+        <div class="container header-texto ">
+          <h1 class="display-1">DibeAnime!</h1>
+          <p class="lead">Información de Anime en un solo lugar.</p>
+          <div className="scroll-icon">&or;</div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
