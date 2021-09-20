f16f787242c34fbff7e639453ff857598d4df2c6
Franco Brumatti <franco.brumatti@gmail.com>
2021-09-20 15:42:35 -0300

    proyectada la navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
-  return <h1>Navbar tomi</h1>;
+  return (
+    <div className="navbar">
+        <div className="navbar-logo">
+          <h1>logo</h1>
+        </div>
+        <div className="navbar-botones">
+          <h1>botones</h1>
+          <h1>botones</h1>
+          <h1>botones</h1>
+        </div>
+    </div>
+  );
 };
 
 export default Navbar;
