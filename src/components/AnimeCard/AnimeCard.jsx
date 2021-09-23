74817260e84bf099cb6cb9e50b677e06a7337f83
Franco Brumatti <franco.brumatti@gmail.com>
2021-09-23 19:11:48 -0300

    cards planteadas

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import './AnimeCard.css';
-
+import './AnimeCard.css'
 
 const AnimeCard = ({ anime }) => {
   return (
-      <div className="animecard">
-        <img className="animecard-img" src={anime.image_url} alt="" />
-        <div className="animecard-body">
-          <p className="animecard-text">{anime.mal_id}</p>
+    <div className="col-lg-3">
+      <div className="card h-100 border-0">
+        <div className="animecard-titulo">
+         <p className="card-text text-center">{anime.title}</p>
+        </div>
+        <img className="card-img h-100" src={anime.image_url} alt="" />
+        <div className="card-img-overlay h-100 animecard-overlay">
+          <p className="card-text">{anime.mal_id}</p>
         </div>
       </div>
+    </div>
   );
 };
 
