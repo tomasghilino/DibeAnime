e56a6f2f7be27c7241aebff1bc2a9cd8afecec59
Franco Brumatti <franco.brumatti@gmail.com>
2021-09-24 17:36:53 -0300

    terminadas las cards sin llamada a la API

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import './AnimeCard.css'
+import './AnimeCard.css';
 
 const AnimeCard = ({ anime }) => {
   return (
     <div className="col-lg-3">
       <div className="card h-100 border-0">
-        <div className="animecard-titulo">
-         <p className="card-text text-center">{anime.title}</p>
-        </div>
+        <p className="card-text text-center fw-bold lh-1">{anime.title}</p>
         <img className="card-img h-100" src={anime.image_url} alt="" />
-        <div className="card-img-overlay h-100 animecard-overlay">
-          <p className="card-text">{anime.mal_id}</p>
+        <div className="card-img-overlay h-100 animecard-overlay align-items-center"> 
+          <div className="d-flex justify-content-between">
+            <p className="card-text">{anime.mal_id}</p>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+              Mas Información
+            </button>
+          </div>
         </div>
       </div>
     </div>
