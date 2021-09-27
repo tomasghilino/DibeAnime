82fdae215649a535e84b2aea171c8d3d5c4ba6c2
Tomas Ghilino <tomasghilino01@gmail.com>
2021-09-27 15:09:00 -0300

    animacion cartas

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -7,17 +7,19 @@ const AnimeCard = ({ anime }) => {
   const [modalShow, setModalShow] = useState(false);
   return (
     <div className="col-lg-3">
-      <div className="card h-100 border border-secondary bg-dark">
-        <p className="card-text text-center text-white fw-bold lh-1 mt-2 mb-2">{anime.title}</p>
-        <img className="card-img h-100 rounded-0" src={anime.image_url} alt="" />
+      <div className="card h-100 border border-secondary bg-dark anime-hover">
+        <p className="card-text text-center text-white fw-bold lh-1 mt-2 mb-2">
+          {anime.title}
+        </p>
+        <img
+          className="card-img h-100 rounded-0"
+          src={anime.image_url}
+          alt=""
+        />
         <div className="card-img-overlay h-100 animecard-overlay align-items-center">
           <div className="d-block">
-            <p className="m-0 fw-bold">
-              &#11088;{anime.score}
-            </p>
-            <p className="mb-2 fw-bold">
-              Episodios: {anime.episodes}
-            </p>
+            <p className="m-0 fw-bold">&#11088;{anime.score}</p>
+            <p className="mb-2 fw-bold">Episodios: {anime.episodes}</p>
             <Button variant="primary" onClick={() => setModalShow(true)}>
               Ver Información
             </Button>
